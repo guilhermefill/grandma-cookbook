4b25bd08d5741adc5991bca1f79276dca865333e
Jose Antonio Carmona <jacarmonamartin@gmail.com>
2022-03-01 20:04:36 +0100

    comments with Pierre recommendations

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -14,7 +14,7 @@ const recipeSchema = new Schema({
     default: "https://img.freepik.com/free-photo/frying-pan-empty-with-various-spices-black-table_1220-561.jpg?t=st=1646052931~exp=1646053531~hmac=6bb2c3c79673febfb82d1725483fab082dc8e1d50885e64bf8ef9e8b607a9d5c&w=1380"
   },
   cookingSteps: {
-    type: String,
+    type: String, //should be an array so we can add step by step
     required: true
   },
   dietRestriction: {
@@ -38,7 +38,7 @@ const recipeSchema = new Schema({
   //   min: 0
   // },
   public: Boolean,
-  comment: String
+  comment: String //mixedobjectId from mongodb or create an object with different keys and types. check with mongoose documentation how to save an object inside schema
 },
 {
     timestamps: true
