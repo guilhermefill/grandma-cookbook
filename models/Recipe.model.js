32545088e8eba479ccdbf5ad6334d754d533e125
Jose Antonio Carmona <jacarmonamartin@gmail.com>
2022-03-01 13:33:18 +0100

    Add user model, recipe model and initial data for DB

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -9,7 +9,7 @@ const recipeSchema = new Schema({
   ingredients: [String],
   creator: String, //Need to link the user here
   shortDescription: String,
-  image: {
+  imageUrl: {
     type: String,
     default: "https://img.freepik.com/free-photo/frying-pan-empty-with-various-spices-black-table_1220-561.jpg?t=st=1646052931~exp=1646053531~hmac=6bb2c3c79673febfb82d1725483fab082dc8e1d50885e64bf8ef9e8b607a9d5c&w=1380"
   },
@@ -18,7 +18,7 @@ const recipeSchema = new Schema({
     required: true
   },
   dietRestriction: {
-      type: String,
+      type: [String],
       enum: ['Vegan', 'Vegetarian', 'Gluten-free']
   },
   level: {
@@ -31,12 +31,13 @@ const recipeSchema = new Schema({
   },
   dishType: {
     type: String,
-    enum: ['Breakfast', 'Main course', 'Soup', 'Snack', 'Drink', 'Dessert', 'Salad', 'Appetizer', 'Starter', 'Other']
+    enum: ['Breakfast', 'Main course', 'Soup', 'Snack', 'Drink', 'Dessert', 'Salad', 'Appetizer', 'Starter', 'Side dish', 'Other']
   },
-  duration: {
-    type: Number,
-    min: 0
-  }
+  // duration: {
+  //   type: Number,
+  //   min: 0
+  // },
+  public: Boolean
 },
 {
     timestamps: true
