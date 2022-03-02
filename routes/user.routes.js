7763949a437fa09f9ce4d63bddd864c8c42cf224
guilhermefill <guilhermefill@gmail.com>
2022-03-02 17:25:53 +0100

    added logout to index and navbar

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,20 +1,20 @@
 const router = require("express").Router();
 
 const isLoggedIn = (req, res, next) => {
-  if(!req.session.currentUser) {
+  if (!req.session.currentUser) {
     res.redirect('/login');
   } else {
     next();
   }
 }
 
-router.get("/main", (_, res) => {
+router.get("/main", (_req, res) => {
   res.render("user-views/main");
 });
 
-router.get("/my-cookbook", (_, res) => {
+router.get("/my-cookbook",isLoggedIn, (_req, res) => {
   res.render("user-views/my-cookbook");
 });
 
+
 module.exports = router;
-module.exports = isLoggedIn;
