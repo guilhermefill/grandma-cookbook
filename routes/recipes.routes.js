1de6de43cb5e943de3994ea0ec19b230cc73c698
guilhermefill <guilhermefill@gmail.com>
2022-03-03 20:26:17 +0100

    final search route

--- routes/recipes.routes.js
+++ routes/recipes.routes.js
@@ -20,21 +20,47 @@ router.get("/search", isLoggedIn, (_, res) => {
 
 router.post('/search', (req, res) => {
   const { searchValue, dietType, level } = req.body;
-  if (dietType !== "" && level !== "") {
+  if (searchValue === "") {
+    res.render("recipe-views/search", { errorMessage: "Please provide a search value" })
+  } else if (dietType !== "" && level !== "") {
     Recipe.find({ $and: [{ dietRestriction: dietType }, { $text: { $search: JSON.stringify(searchValue) } }, { level: level }] })
-      .then(foundRecipe => res.send(foundRecipe))
+      .then(foundRecipe => {
+        if (foundRecipe.lenght === 0) {
+          res.render('recipe-views/search-result', { errorMessage: "Nothing was found" })
+        } else {
+          res.render('recipe-views/search-result', { recipes: foundRecipe })
+        }
+      })
       .catch(error => console.log(error));
   } else if (level !== "") {
     Recipe.find({ $and: [{ level: level }, { $text: { $search: JSON.stringify(searchValue) } }] })
-      .then(foundRecipe => res.send(foundRecipe))
+      .then(foundRecipe => {
+        if (foundRecipe.lenght === 0) {
+          res.render('recipe-views/search-result', { errorMessage: "Nothing was found" })
+        } else {
+          res.render('recipe-views/search-result', { recipes: foundRecipe })
+        }
+      })
       .catch(error => console.log(error));
   } else if (dietType !== "") {
     Recipe.find({ $and: [{ dietRestriction: dietType }, { $text: { $search: JSON.stringify(searchValue) } }] })
-      .then(foundRecipe => res.send(foundRecipe))
+      .then(foundRecipe => {
+        if (foundRecipe.lenght === 0) {
+          res.render('recipe-views/search-result', { errorMessage: "Nothing was found" })
+        } else {
+          res.render('recipe-views/search-result', { recipes: foundRecipe })
+        }
+      })
       .catch(error => console.log(error));
   } else {
     Recipe.find({ $text: { $search: JSON.stringify(searchValue) } })
-      .then(foundRecipe => res.send(foundRecipe))
+      .then(foundRecipe => {
+        if (foundRecipe.lenght === 0) {
+          res.render('recipe-views/search-result', { errorMessage: "Nothing was found" })
+        } else {
+          res.render('recipe-views/search-result', { recipes: foundRecipe })
+        }
+      })
       .catch(error => console.log(error));
   }
 })
