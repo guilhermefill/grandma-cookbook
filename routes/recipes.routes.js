26bbee083b2b185378429123b459552e9d1a6164
guilhermefill <guilhermefill@gmail.com>
2022-03-05 16:15:50 +0100

    add to cookbook functionality

--- routes/recipes.routes.js
+++ routes/recipes.routes.js
@@ -70,22 +70,54 @@ router.get("/create", isLoggedIn, (req, res) => {
   res.render("recipe-views/create");
 });
 
-router.get("/detail/:id", isLoggedIn, (req, res) => {
+router.get("/detail/:id", isLoggedIn, async (req, res) => {
   const user = req.session.currentUser;
   const { id } = req.params;
-  Recipe.findById(id).populate('creator')
-    .then(recipe => {
-      if (user.username === recipe.creator[0].username) {
-        res.render("recipe-views/detail", { recipe: recipe, userMatch: true });
-      } else {
-        res.render("recipe-views/detail", { recipe: recipe }); //TODO add a else if for if user comment creator === user view comments
-      }
-    })
+  try {
+    const foundNotes = await Note.find({ $and: [{ user: user._id }, { recipe: id }] })
+    const recipe = await Recipe.findById(id).populate('creator')
+    if (user.username === recipe.creator[0].username) {
+      res.render("recipe-views/detail", { recipe: recipe, userMatch: true, note: foundNotes });
+    } else {
+      res.render("recipe-views/detail", { recipe: recipe, notes: foundNotes });
+    }
+  } catch (error) {
+    console.log(error)
+  }
+});
+
+router.post('/add-note/:id', isLoggedIn, (req, res) => {
+  const user = req.session.currentUser;
+  const { id } = req.params;
+  const { note } = req.body;
+  Note.create({ notes: note, user: user._id, recipe: id })
+    .then(createdNote => res.redirect(`/recipe/detail/${id}`))
+    .catch(error => console.log(error));
+});
+
+router.post('/delete-note/:id', isLoggedIn, (req, res) => {
+  const user = req.session.currentUser;
+  const { id } = req.params;
+  const { noteId } = req.body;
+  Note.findByIdAndDelete(noteId)
+    .then(() => res.redirect(`/recipe/detail/${id}`))
+    .catch(error => console.log(error));
+});
+
+router.post('/add-to-cookbook/:id', isLoggedIn, (req, res) => {
+  const user = req.session.currentUser;
+  const { id } = req.params;
+  User.findByIdAndUpdate(user._id, { $push: { cookbook: id } })
+    .then(() => res.redirect(`/my-cookbook`))
     .catch(error => console.log(error));
 });
 
 router.get("/edit/:id", isLoggedIn, (req, res) => {
   res.render("recipe-views/edit");
 });
+
+router.post("/delete/:id", isLoggedIn, (req, res) => {
+  res.redirect('/my-cookbook')
+})
 
 module.exports = router;
