3e07dd4ac344e9f6ed292a9ca67834bc73fecbad
Jose Antonio Carmona <jacarmonamartin@gmail.com>
2022-03-07 16:53:34 +0100

    play around the create view

---
+++ public/js/create-recipe.js
@@ -0,0 +1,26 @@
+// const newInput = document.querySelector("#add-new-form > input");
+// const ingredientList = document.getElementById("ingredient-list");
+
+// const createButton = (textContent, callback) => {
+//   const buttonElement = document.createElement("button");
+//   buttonElement.textContent = textContent;
+//   buttonElement.onclick = callback;
+//   return buttonElement;
+// };
+// const createNewBox = (text) => {
+//   const div = document.createElement("div");
+//   div.innerText = text;
+//   return div;
+// };
+
+// const addNewItem = () => {
+//   const inputValue = newInput.value;
+//   const item = createNewBox(inputValue);
+//   ingredientList.appendChild(item);
+// };
+
+
+// newInput.addEventListener('keydown', (event) => {
+//   if (event.key === "Enter") {
+//   }
+// });
